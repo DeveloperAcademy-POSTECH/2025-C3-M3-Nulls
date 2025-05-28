b61784fda39b9174d10c043374cc45938be60385
Yang Sijun <yang@sijun.dev>
2025-05-28 22:18:29 +0900

    chore: Create new xcode project

---
+++ Projects/medimo/medimoTests/medimoTests.swift
@@ -0,0 +1,17 @@
+//
+//  medimoTests.swift
+//  medimoTests
+//
+//  Created by 양시준 on 5/28/25.
+//
+
+import Testing
+@testable import medimo
+
+struct medimoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
