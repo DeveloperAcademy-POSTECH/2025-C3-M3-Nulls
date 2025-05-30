a2f329b3952a156f029e906f3d8fe7fcfef79eaa
Yang Sijun <yang@sijun.dev>
2025-05-30 18:20:26 +0900

    feat: Glossary 목록 조회

---
+++ Projects/Medimo/Sources/GlossaryList/View/GlossaryListView.swift
@@ -0,0 +1,30 @@
+//
+//  GlossaryListVie.swift
+//  Projects
+//
+//  Created by 양시준 on 5/30/25.
+//
+
+import SwiftUI
+import CoreData
+
+struct GlossaryListView: View {
+    @Environment(\.managedObjectContext) var context
+    @State var viewModel: GlossaryListViewModel
+    
+    init(context: NSManagedObjectContext) {
+        _viewModel = State(wrappedValue: GlossaryListViewModel(context: context))
+    }
+    
+    var body: some View {
+        List {
+            ForEach(viewModel.glossaries) { glossary in
+                Text(glossary.title)
+            }
+        }
+    }
+}
+
+#Preview {
+    GlossaryListView(context: PersistenceController.preview.container.viewContext)
+}
