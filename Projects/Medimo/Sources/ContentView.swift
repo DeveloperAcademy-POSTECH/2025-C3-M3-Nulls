418c7a20077b27ebc454f26171c06cc026a9bf95
Yang Sijun <yang@sijun.dev>
2025-06-01 22:56:19 +0900

    feat: TabView 초기 뷰를 StudyView로 설정

--- Projects/Medimo/Sources/ContentView.swift
+++ Projects/Medimo/Sources/ContentView.swift
@@ -1,18 +1,21 @@
 import SwiftUI
 
- public struct ContentView: View {
-     @Environment(\.managedObjectContext) var context
+public struct ContentView: View {
+    @Environment(\.managedObjectContext) var context
+    @State private var selectedTab: TabType = .study
 
      public var body: some View {
-         TabView {
-             Tab("용어집", systemImage: "book") {
+         TabView(selection: $selectedTab) {
+             Tab("용어집", systemImage: "book", value: .glossary) {
                  GlossaryListView(context: context)
              }
-             Tab("학습", systemImage: "book") {
+             
+             Tab("학습", systemImage: "book", value: .study) {
                  // TODO: StudyManager 구현하여 수정해야 함
                  StudyView(glossary: try! context.fetch(Glossary.fetchRequest())[0])
              }
-             Tab("사전", systemImage: "book") {
+             
+             Tab("사전", systemImage: "book", value: .dictionary) {
                  DictionaryView()
              }
          }
