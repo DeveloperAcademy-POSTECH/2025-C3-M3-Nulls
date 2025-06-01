805266941929485739a1ee156cf3572eef582407
Yang Sijun <yang@sijun.dev>
2025-06-01 22:41:42 +0900

    feat: TabView 구현

--- Projects/Medimo/Sources/ContentView.swift
+++ Projects/Medimo/Sources/ContentView.swift
@@ -3,9 +3,23 @@ import SwiftUI
  public struct ContentView: View {
      @Environment(\.managedObjectContext) var context
 
-     public init() {}
-
      public var body: some View {
-         GlossaryListView(context: context)
+         TabView {
+             Tab("용어집", systemImage: "book") {
+                 GlossaryListView(context: context)
+             }
+             Tab("학습", systemImage: "book") {
+                 // TODO: StudyManager 구현하여 수정해야 함
+                 StudyView(glossary: try! context.fetch(Glossary.fetchRequest())[0])
+             }
+             Tab("사전", systemImage: "book") {
+                 DictionaryView()
+             }
+         }
      }
  }
+
+#Preview {
+    ContentView()
+        .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+}
