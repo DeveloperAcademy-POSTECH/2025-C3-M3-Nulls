285f17d18fd2e1084797c08b1bae4fe5dd55b273
Yang Sijun (Air) <yang@sijun.dev>
2025-06-02 09:30:06 +0900

    [feat] TabView 및 네비게이션 구현 (#25)
    * feat: TabView 구현
    
    * feat: TabView 초기 뷰를 StudyView로 설정

--- Projects/Medimo/Sources/ContentView.swift
+++ Projects/Medimo/Sources/ContentView.swift
@@ -1,11 +1,28 @@
 import SwiftUI
 
- public struct ContentView: View {
-     @Environment(\.managedObjectContext) var context
-
-     public init() {}
+public struct ContentView: View {
+    @Environment(\.managedObjectContext) var context
+    @State private var selectedTab: TabType = .study
 
      public var body: some View {
-         GlossaryListView(context: context)
+         TabView(selection: $selectedTab) {
+             Tab("용어집", systemImage: "book", value: .glossary) {
+                 GlossaryListView(context: context)
+             }
+             
+             Tab("학습", systemImage: "book", value: .study) {
+                 // TODO: StudyManager 구현하여 수정해야 함
+                 StudyView(glossary: try! context.fetch(Glossary.fetchRequest())[0])
+             }
+             
+             Tab("사전", systemImage: "book", value: .dictionary) {
+                 DictionaryView()
+             }
+         }
      }
  }
+
+#Preview {
+    ContentView()
+        .environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+}
