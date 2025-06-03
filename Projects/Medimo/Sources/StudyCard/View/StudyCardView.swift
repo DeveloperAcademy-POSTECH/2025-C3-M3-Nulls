ec3996b83fc682713169c56d277b86dec244cf69
Hyeongi Kim (Wade) <61077215+hgkim215@users.noreply.github.com>
2025-06-03 13:42:27 +0900

    feat: 네비게이션 매니저 구현 (#39)
    * [Feat] #28 NavigationManager 구현
    
    * [Feat] #28 NavigationManager 구현

--- Projects/Medimo/Sources/StudyCard/View/StudyCardView.swift
+++ Projects/Medimo/Sources/StudyCard/View/StudyCardView.swift
@@ -4,72 +4,72 @@
 //
 //  Created by 이서현 on 6/1/25.
 //
-import SwiftUI
 import CoreData
+import SwiftUI
 
 struct StudyCardView: View {
     @Environment(\.managedObjectContext) private var context
-    
+
     @Bindable private var viewModel: StudyCardViewModel
     @State private var index: Int
-    
+
     init(glossary: Glossary) {
         _viewModel = .init(wrappedValue: StudyCardViewModel(glossary: glossary, studyTermSize: 5))
         index = 5
     }
-    
+
     var body: some View {
-        NavigationStack {
-            VStack {
-                HStack {
-                    ProgressView(value: Double(index) / Double(viewModel.studyTermSize))
-                                .progressViewStyle(LinearProgressViewStyle(tint: .blue))
-                                .padding(.trailing)
-                    
-                    Text("\(String(format: "%02d", index)) / \(viewModel.studyTermSize)")
-                }
-                .padding(.bottom)
-                
-                TermCardView(term: viewModel.studyTerms[index - 1])
-                    .gesture(
-                        DragGesture()
-                            .onEnded { value in
-                                let horizontalAmount = value.translation.width
-                                
-                                if horizontalAmount < -50, index < viewModel.studyTermSize {
-                                    index += 1
-                                } else if horizontalAmount > 50, index > 1 {
-                                    index -= 1
-                                }
-                            }
-                    )
-                
-                Spacer()
-                
-                Button("문제 풀기") {
-                    // TODO: 액션 정의
-                }
-                .frame(width: 220)
-                .padding(.vertical, 14)
-                .padding(.horizontal, 20)
-                .background(Color("Navy"))
-                .foregroundColor(.white)
-                .cornerRadius(10)
-                .shadow(radius: 3)
-                .opacity(viewModel.studyTermSize == index ? 1 : 0)
-                
-                Spacer()
+//        NavigationStack {
+        VStack {
+            HStack {
+                ProgressView(value: Double(index) / Double(viewModel.studyTermSize))
+                    .progressViewStyle(LinearProgressViewStyle(tint: .blue))
+                    .padding(.trailing)
+
+                Text("\(String(format: "%02d", index)) / \(viewModel.studyTermSize)")
             }
-            .padding(.horizontal, 40)
-            .padding(.bottom, 20)
+            .padding(.bottom)
+
+            TermCardView(term: viewModel.studyTerms[index - 1])
+                .gesture(
+                    DragGesture()
+                        .onEnded { value in
+                            let horizontalAmount = value.translation.width
+
+                            if horizontalAmount < -50, index < viewModel.studyTermSize {
+                                index += 1
+                            } else if horizontalAmount > 50, index > 1 {
+                                index -= 1
+                            }
+                        }
+                )
+
+            Spacer()
+
+            Button("문제 풀기") {
+                // TODO: 액션 정의
+            }
+            .frame(width: 220)
+            .padding(.vertical, 14)
+            .padding(.horizontal, 20)
+            .background(Color("Navy"))
+            .foregroundColor(.white)
+            .cornerRadius(10)
+            .shadow(radius: 3)
+            .opacity(viewModel.studyTermSize == index ? 1 : 0)
+
+            Spacer()
         }
+        .padding(.horizontal, 40)
+        .padding(.bottom, 20)
     }
+//    }
 }
 
 #Preview {
     let context = PersistenceController.preview.container.viewContext
     let glossary = try! context.fetch(Glossary.fetchRequest())[0]
-    
+
     StudyCardView(glossary: glossary)
         .environment(\.managedObjectContext, context)
 }
