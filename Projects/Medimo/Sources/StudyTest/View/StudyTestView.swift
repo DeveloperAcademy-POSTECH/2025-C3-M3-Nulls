8ad5840904d201568709e94b5976515a07803ff1
nuyhhyun <sarahs19991115@gmail.com>
2025-06-06 01:18:42 +0900

    feat: 다음 및 결과 버튼 컴포넌트화 및 적용

--- Projects/Medimo/Sources/StudyTest/View/StudyTestView.swift
+++ Projects/Medimo/Sources/StudyTest/View/StudyTestView.swift
@@ -12,7 +12,7 @@ struct StudyTestView: View {
     @Environment(\.managedObjectContext) private var context
     
     private var viewModel: StudyTestViewModel
-    @State private var index: Int = 1
+    @State private var index: Int = 5
     
     var terms: [Term]
     var studyTermSize: Int {
@@ -29,15 +29,21 @@ struct StudyTestView: View {
             ProgressBar(index: index, total: terms.count)
                 .padding(.bottom, 48)
                 .padding(.horizontal, 8)
+            
             // TODO: 문제 넘어갈 때마다 문제 바뀌는 것 다른 걸로 구현
-            //            SpellingTestView(term: terms[0])
+//            SpellingTestView(term: terms[0])
 //            MeaningTestView(term: terms[0])
 //            AbbreviationTestView(term: terms[0])
             PronounciationTestView(term: terms[0])
+            
             CorrectAnswer()
             // TODO: 문제 유형에 따라 다른 답 넘기기
             WrongAnswer(correctAnswer: terms[0].spelling ?? "")
             Spacer()
+            // TODO: 답 입력하면 나타나게 설정
+//            NextButton(buttonText: "다음 문제로")
+            NextButton(buttonText: "학습 결과 보러가기")
+                .opacity(index == studyTermSize ? 1 : 0)
         }
         .padding(24)
         .background(AppColor.bgColor.ignoresSafeArea())
