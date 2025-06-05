df5c2596852bf0e942dacad546f4f2973060cc2c
bear <duilwang@naver.com>
2025-06-05 17:01:27 +0900

    design ui 해결

--- Projects/Medimo/Sources/Dictionary/View/SubView/DictionaryViewComponents.swift
+++ Projects/Medimo/Sources/Dictionary/View/SubView/DictionaryViewComponents.swift
@@ -1,4 +1,3 @@
-
 //  DictionaryComponentsView.swift
 //  Medimo
 //
@@ -27,18 +26,20 @@ struct DictionaryViewComponents {
                             prompt: Text("단어를 입력하세요")
                                 .font(.caption)
                         )
-                        .foregroundColor(AppColor.label)
+                        .foregroundStyle(AppColor.label)
                         .padding(21)
                     )
                     .foregroundStyle(Color.white)
                     .frame(height: 50)
                 
                 Image(systemName: "magnifyingglass")
-                    .foregroundColor(.white)
+                    .foregroundStyle(.white)
                     .padding(10)
                     .background(AppColor.secondary)
                     .clipShape(RoundedRectangle(cornerRadius: 16))
-                    .frame(width: 361, height: 50, alignment: .trailing)
+                    .padding(.trailing, 10)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    
             }
             .padding(.horizontal, 16)
             .padding(.bottom, 20)
@@ -57,7 +58,7 @@ struct DictionaryViewComponents {
                     VStack(spacing: 0) {
                         Text(term.spelling ?? "")
                             .font(.subheadlineEng)
-                            .foregroundColor(AppColor.label)
+                            .foregroundStyle(AppColor.label)
                             .frame(maxWidth: .infinity, alignment: .leading)
                             .padding(.top, 16)
                             .padding(.bottom, 8)
@@ -66,11 +67,11 @@ struct DictionaryViewComponents {
                             if let abbreviation = term.abbreviation {
                                 Text("[ \(abbreviation) ]")
                                     .font(.subheadlineEng)
-                                    .foregroundColor(AppColor.label)
+                                    .foregroundStyle(AppColor.label)
                             }
                             Text(term.meaning ?? "")
                                 .font(.caption)
-                                .foregroundColor(AppColor.secondaryLabel)
+                                .foregroundStyle(AppColor.secondaryLabel)
                             Spacer()
                         }
                         .padding(.bottom, 8)
@@ -81,7 +82,7 @@ struct DictionaryViewComponents {
                         .frame(height: 1)
                         .padding(.horizontal, 12)
                         .cornerRadius(2)
-                        .foregroundColor(AppColor.grey2)
+                        .foregroundStyle(AppColor.grey2)
                 }
                 .listStyle(.plain)
                 .listRowInsets(EdgeInsets())
@@ -95,84 +96,4 @@ struct DictionaryViewComponents {
     }
 }
 
-
-#Preview("Search + List") {
-    struct PreviewWrapper: View {
-        @State private var searchText = ""
-        @State private var selectedTerm: Term? = nil
-        let viewModel: DictionaryViewModel = {
-            let context = PersistenceController.preview.container.viewContext
-            let term1 = Term(context: context)
-            term1.spelling = "Cardiology"
-            term1.abbreviation = "Cardio"
-            term1.meaning = "Study of the heart"
-
-            let term2 = Term(context: context)
-            term2.spelling = "Neurology"
-            term2.abbreviation = "Neuro"
-            term2.meaning = "Study of the brain and nervous system"
-            
-            let term3 = Term(context: context)
-            term2.spelling = "Neurology"
-            term2.abbreviation = "Neuro"
-            term2.meaning = "Study of the brain and nervous system"
-            let term4 = Term(context: context)
-            term2.spelling = "Neurology"
-            term2.abbreviation = "Neuro"
-            term2.meaning = "Study of the brain and nervous system"
-            let term5 = Term(context: context)
-            term2.spelling = "Neurology"
-            term2.abbreviation = "Neuro"
-            term2.meaning = "Study of the brain and nervous system"
-            let term6 = Term(context: context)
-            term2.spelling = "Neurology"
-            term2.abbreviation = "Neuro"
-            term2.meaning = "Study of the brain and nervous system"
-            let term7 = Term(context: context)
-            term2.spelling = "Neurology"
-            term2.abbreviation = "Neuro"
-            term2.meaning = "Study of the brain and nervous system"
-            
-            let term8 = Term(context: context)
-            term2.spelling = "Neurology"
-            term2.abbreviation = "Neuro"
-            term2.meaning = "Study of the brain and nervous system"
-            let term9 = Term(context: context)
-            term2.spelling = "Neurology"
-            term2.abbreviation = "Neuro"
-            term2.meaning = "Study of the brain and nervous system"
-            let term10 = Term(context: context)
-            term2.spelling = "Neurology"
-            term2.abbreviation = "Neuro"
-            term2.meaning = "Study of the brain and nervous system"
-            let term11 = Term(context: context)
-            term2.spelling = "Neurology"
-            term2.abbreviation = "Neuro"
-            term2.meaning = "Study of the brain and nervous system"
-            let term12 = Term(context: context)
-            term2.spelling = "Neurology"
-            term2.abbreviation = "Neuro"
-            term2.meaning = "Study of the brain and nervous system"
-
-            let vm = DictionaryViewModel(context: context)
-            vm.term = [term1, term2, term3, term4, term5, term6, term7, term8, term9, term10, term11, term12]
-            return vm
-        }()
-
-        var body: some View {
-            ZStack{
-                Color(AppColor.secondarySystemFill)
-                    .ignoresSafeArea(edges: .top)
-                
-                VStack {
-                    DictionaryViewComponents.searchHeader(searchText: $searchText)
-                    DictionaryViewComponents.termList(viewModel: viewModel, selectedTerm: $selectedTerm, searchText: searchText)
-                }
-                .padding()
-            }
-        }
-    }
-
-    return PreviewWrapper()
-}
 
